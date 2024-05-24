775078cc81bb51af21c49f13f792705bc6e45a03
tsukada-y <taro-y@example.com>
2024-05-24 16:43:31 +0900

    quiz1.html, QuizControllerの変更

--- SpringBoot-teamFriends/src/main/java/com/example/demo/quiz/QuizController.java
+++ SpringBoot-teamFriends/src/main/java/com/example/demo/quiz/QuizController.java
@@ -1,5 +1,15 @@
 package com.example.demo.quiz;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class QuizController {
+	@RequestMapping("/quiz1")
+	public String top(Model model) {
+		return "quiz/quiz1";
+	}
+	
 
 }
