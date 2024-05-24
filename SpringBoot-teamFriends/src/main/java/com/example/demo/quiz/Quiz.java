84a884335485c161ecdc811f09e0cea7df47ece1
miyamamo <y@example.com>
2024-05-24 16:47:12 +0900

    test

--- SpringBoot-teamFriends/src/main/java/com/example/demo/quiz/Quiz.java
+++ SpringBoot-teamFriends/src/main/java/com/example/demo/quiz/Quiz.java
@@ -1,5 +1,19 @@
 package com.example.demo.quiz;
 
 public class Quiz {
+	
+		private String question;
 
+		public Quiz() {}
+
+		public String getQuestion1() {
+			return getQuestion1();
+		}
+		public void setquestion1(String question1) {
+			this.question1 = question1;
+		}
 }
+	
+
+
+
--- SpringBoot-teamFriends/src/main/java/com/example/demo/quiz/QuizController.java
+++ SpringBoot-teamFriends/src/main/java/com/example/demo/quiz/QuizController.java
@@ -1,5 +1,15 @@
 package com.example.demo.quiz;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class QuizController {
+
+	@RequestMapping("/start")
+	public String top(Model model) {
+		model.addAttribute("title", "クイズ"); 
+		return "index";
+	}
+
 
 }
