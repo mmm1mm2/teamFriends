9c81a009d9f275ec79783eced1b068bf9a768e1e
miyamamo <y@example.com>
2024-05-27 14:04:03 +0900

    あああ

--- SpringBoot-teamFriends/src/main/java/com/example/demo/entity/Entquiz.java
+++ SpringBoot-teamFriends/src/main/java/com/example/demo/entity/Entquiz.java
@@ -1,21 +1,33 @@
 package com.example.demo.entity;
 
-public class Entquiz {
+public class EntQuiz {
+
+	private int id;
 	private String name;
-	private String question;
-	
+
+	public EntQuiz() {};
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getQuestion() {
+	
+	public int getQuestion() {
 		return question;
 	}
+
 	public void setQuestion(String question) {
 		this.question = question;
 	}
-	
-
 }
